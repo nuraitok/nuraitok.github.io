3cc55cc49854db927a944fd834c440fb816ba976
nuraitok <164940322+nuraitok@users.noreply.github.com>
2024-03-26 02:03:49 +0600

    script.js

--- script.js
+++ script.js
@@ -15,19 +15,19 @@ document.addEventListener('DOMContentLoaded', function(){
 
         if (error === 0){
             form.classList.add('_sending');
-            // let response = await fetch('sendmail.php', {
-            //     method: 'POST',
-            //     body: formData
-            // });
-            // if(response.ok){
-            //     let result = await response.json();
-            //     alert(result.message);
-            //     form.reset();
-            //     form.classList.remove('_sending');
-            // }else {
-            //     alert('Ошибка');
-            //     form.classList.remove('_sending');
-            // }
+             let response = await fetch('sendmail.php', {
+                 method: 'POST',
+                 body: formData
+             });
+             if(response.ok){
+                 let result = await response.json();
+                 alert(result.message);
+                 form.reset();
+                form.classList.remove('_sending');
+             }else {
+                 alert('Ошибка');
+                 form.classList.remove('_sending');
+             }
         }
         else{
             alert('Заполните обязательные поля')
